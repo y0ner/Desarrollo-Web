392bd7c2cff26394c9dcf0a40cfaf451f2fc17f3
yoner <yonerisaza0@gmail.com>
2025-09-24 09:49:06 -0500

    9:49am

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,8 +1,6 @@
-import morgan from 'morgan';
-
-
-import express from "express";
-import type { Application } from "express";
+import express, { Application } from "express";
+import morgan from "morgan";
+import { sequelize } from "../database/db";
 
 // Load environment variables from the .env file
 
@@ -13,6 +11,8 @@ export class App {
     this.app = express();
     this.settings();
     this.middlewares();
+    this.dbConnection(); // Call the database connection method
+
   }
 
   // Application settings
@@ -20,16 +20,27 @@ export class App {
     this.app.set('port', this.port || process.env.PORT || 4000);
   }
 
+ // Middleware configuration
+ private middlewares(): void {
+  this.app.use(morgan('dev'));
+  this.app.use(express.json()); // leer json raw
+  this.app.use(express.urlencoded({ extended: false })); //leer json form
+}
 
-  // Start the server
+
+// Method to connect and synchronize the database
+private async dbConnection(): Promise<void> {
+  try {
+    await sequelize.sync({ force: true }); // Synchronize the database
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+// Start the server
   async listen() {
     await this.app.listen(this.app.get('port'));
     console.log('Server on port', this.app.get('port'));
   }
-
-  private middlewares() {
-        this.app.use(morgan('dev'));
-        this.app.use(express.json()); // leer json raw
-        this.app.use(express.urlencoded({ extended: false })); //leer json form
-}
 }
