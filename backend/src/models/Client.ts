db12eaef9a5ba0908527b27304c9d15e36f8b979
yoner <yonerisaza0@gmail.com>
2025-10-01 10:54:31 -0500

    10:54am

--- backend/src/models/Client.ts
+++ backend/src/models/Client.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from "sequelize";
-import { sequelize } from "../database/db";
+import sequelize from "../database/connection";
 import { Sale } from "./Sale";
 
 export interface ClientI {
--- backend/src/models/Sale.ts
+++ backend/src/models/Sale.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from "sequelize";
-import { sequelize } from "../database/db";
+import sequelize from "../database/connection";
 
 export interface SaleI {
   id?: number;
